66b4b62f7d4acb59550cabadf9ae0c021a5f6e3f
Anna Bai <anna.bai.6512@gmail.com>
2019-09-21 15:00:07 -0500

    Updates

---
+++ precache-manifest.207f537900f728a7bf262bff78c049c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a00f16dc120da5c815b5f38917e441",
+    "url": "/reactWorkshop/index.html"
+  },
+  {
+    "revision": "ec3da0154249183232f9",
+    "url": "/reactWorkshop/static/css/main.9ebf5bb6.chunk.css"
+  },
+  {
+    "revision": "b94e67ab1a1d357c04c2",
+    "url": "/reactWorkshop/static/js/2.2151e755.chunk.js"
+  },
+  {
+    "revision": "ec3da0154249183232f9",
+    "url": "/reactWorkshop/static/js/main.e6351cb1.chunk.js"
+  },
+  {
+    "revision": "00dcecd37b763f5c6cd4",
+    "url": "/reactWorkshop/static/js/runtime-main.e7af3d78.js"
+  }
+]);
