2f6eeb2c5d7c924ed74232f9029da752ba5c4e29
Anna Bai <anna.bai.6512@gmail.com>
2019-09-21 14:53:48 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -11,35 +11,36 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="logo" />
       </header>
-    </div>
-
-    <header className="App-body">
-      <p>
-         Hello! My name is Anna Bai and I'm a freshman at Rice University.
-      </p>
-      <a
-         className="App-link"
-         href="https://www.rice.edu/"
-         target="_blank"
-        rel="noopener noreferrer"
-       >
-        Check out Rice University!
-      </a>
-       <button type="button" onClick={() => {
-         if (count % 2 === 1)
-         {
-           setText("Surprise!")
+      <header className="App-body">
+        <p>
+           Hello! My name is Anna Bai and I'm a freshman at Rice University.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.rice.edu/"
+          target="_blank"
+         rel="noopener noreferrer"
+        >
+         Check out Rice University!
+        </a>
+      </header>
+      <header className="App-button">
+        <button type="button" onClick={() => {
+           if (count % 2 === 1)
+           {
+             setText("Surprise!")
+           }
+           else
+           {
+             setText("")
          }
-         else
-         {
-           setText("")
-        }
-         setCount(count+1)
-        }}>
-          About Me
-        </button>
-      <p>{text}</p>
+          setCount(count+1)
+         }}>
+            About Me
+          </button>
+        <p>{text}</p>
     </header>
+    </div>
   );
 }
 
